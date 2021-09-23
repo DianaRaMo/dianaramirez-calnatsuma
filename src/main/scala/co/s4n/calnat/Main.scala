bbac85322e98f1cbdf3f0dd4193fb50b8d48c413
DianaRaMo <dianaramirez@seven4n.com>
2021-09-23 14:57:04 -0500

    archivosScala

--- src/main/scala/co/s4n/calnat/Main.scala
+++ src/main/scala/co/s4n/calnat/Main.scala
@@ -1,1 +1,32 @@
-123
+package co.s4n.calnat
+
+import scala.io.StdIn
+
+object Main extends App {
+	def leerInt (prompt:String):Int = {
+		val s = StdIn.readLine(prompt)
+		s.toInt
+	}
+
+	def esCero(nat:Nat):Boolean = nat match {
+		case Cero() => true
+		case Suc(nat) => false	
+	}
+
+	def conIntANat (i:Int):Nat = 
+
+	def imprimirNat(nat:Nat):String = 
+
+	def sumaNat(nat1:Nat, nat2:Nat):Nat = nat1 + nat2
+
+	def read(prompt: String): (Int, Int) = {
+      val s = StdIn.readLine(prompt)
+      val ints = strToInts(s)
+      (ints(0), ints(1))
+    }
+
+	val (a, b) = read("");
+
+	
+
+}
--- src/main/scala/co/s4n/calnat/Nat.scala
+++ src/main/scala/co/s4n/calnat/Nat.scala
@@ -1,1 +1,6 @@
-1234
+package co.s4n.calnat
+
+trait Nat
+case class Cero() extends Nat
+case class Suc(nat:Nat) extends calnat
+
